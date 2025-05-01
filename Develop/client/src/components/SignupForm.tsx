fe77b6a95f41e1f07cbe2cbac20af17021c4eaad
gamalmubarak <eng-gemy@hotmail.com>
2025-04-30 22:09:52 -0400

    code updated

--- Develop/client/src/components/SignupForm.tsx
+++ Develop/client/src/components/SignupForm.tsx
@@ -37,7 +37,7 @@ const SignupForm = ({}: { handleModalClose: () => void }) => {
         variables: { ...userFormData },
       });
 
-      if (!response.ok) {
+      if (!response) {
         throw new Error('something went wrong!');
       }
 
