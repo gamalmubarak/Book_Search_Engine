3f5effed574f84945fc53d98f5c42803c2ad17e1
gamalmubarak <eng-gemy@hotmail.com>
2025-04-30 23:20:01 -0400

    code updated

--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -40,13 +40,12 @@ app.use(
 );
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
+  app.use(express.static(path.join(__dirname, '../../client/dist')));
 
   app.get('*', (_req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
   });
 }
-
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.listen(PORT, () => {
