2553c5d70ae8b387fdcccac69624f8b8bc62f699
gamalmubarak <eng-gemy@hotmail.com>
2025-04-30 21:48:43 -0400

    Replace the loginUser()  with the LOGIN_USER mutation functionality

--- Develop/client/src/components/LoginForm.tsx
+++ Develop/client/src/components/LoginForm.tsx
@@ -2,8 +2,8 @@
 import { useState } from 'react';
 import type { ChangeEvent, FormEvent } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { loginUser } from '../utils/API';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 import type { User } from '../models/User';
 
@@ -12,6 +12,7 @@ const LoginForm = ({}: { handleModalClose: () => void }) => {
   const [userFormData, setUserFormData] = useState<User>({ username: '', email: '', password: '', savedBooks: [] });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+  const [loginUser] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
@@ -29,13 +30,15 @@ const LoginForm = ({}: { handleModalClose: () => void }) => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const response = await loginUser({
+        variables: { ...userFormData },
+      });
 
       if (!response.ok) {
         throw new Error('something went wrong!');
       }
 
-      const { token } = await response.json();
+      const { token } = await response.data.login;
       Auth.login(token);
     } catch (err) {
       console.error(err);
