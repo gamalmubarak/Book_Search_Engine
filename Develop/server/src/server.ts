ece6fe4e315e266e9a73398748a5570789cd764e
gamalmubarak <eng-gemy@hotmail.com>
2025-04-30 20:57:58 -0400

    Implement the Apollo Server and apply it to the Express server as middleware.

--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -1,21 +1,54 @@
 import express from 'express';
-import path from 'node:path';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import cors from 'cors';
+import path from 'path';
+import { typeDefs, resolvers } from './schemas/index.js';
 import db from './config/connection.js';
-import routes from './routes/index.js';
+import { authenticateToken } from './services/auth.js';
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
+const startApolloServer = async () => {
+  await server.start();
+  
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+  allowedHeaders: ['Content-Type', 'Authorization'], 
+}));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(
+  '/graphql',
+  expressMiddleware(server, {
+    context: authenticateToken
+  }),
+);
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+
+  app.get('*', (_req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
 }
 
-app.use(routes);
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+app.listen(PORT, () => {
+  console.log(`API server running on port ${PORT}!`);
+  console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+  });
+};
+
+
+startApolloServer();
