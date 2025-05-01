74bcf31986aabb02c9e242a52e69e286eb83c290
gamalmubarak <eng-gemy@hotmail.com>
2025-04-30 21:45:28 -0400

    Replace the addUser()  with the ADD_USER mutation functionality

--- Develop/client/src/components/SignupForm.tsx
+++ Develop/client/src/components/SignupForm.tsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import type { ChangeEvent, FormEvent } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { createUser } from '../utils/API';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 import type { User } from '../models/User';
 
@@ -14,6 +14,8 @@ const SignupForm = ({}: { handleModalClose: () => void }) => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+  // set mutation for adding user
+  const [addUser] = useMutation(ADD_USER);
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
@@ -31,13 +33,15 @@ const SignupForm = ({}: { handleModalClose: () => void }) => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const response = await addUser({
+        variables: { ...userFormData },
+      });
 
       if (!response.ok) {
         throw new Error('something went wrong!');
       }
 
-      const { token } = await response.json();
+      const { token } = await response.data.addUser;
       Auth.login(token);
     } catch (err) {
       console.error(err);
