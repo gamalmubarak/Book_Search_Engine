1848c4acbd12abc90963b27be23f9117bb760cd2
gamalmubarak <eng-gemy@hotmail.com>
2025-04-30 21:23:12 -0400

    REMOVE_BOOK will execute the removeBook mutation

--- Develop/client/src/utils/mutations.ts
+++ Develop/client/src/utils/mutations.ts
@@ -43,3 +43,21 @@ export const SAVE_BOOK = gql`
     }
   }
 `;
+
+export const REMOVE_BOOK = gql`
+  mutation RemoveBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        title
+        authors
+        description
+        image
+        link
+      }
+    }
+  }
+`;
