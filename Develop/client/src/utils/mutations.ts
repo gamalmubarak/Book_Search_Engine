418641dbc39e38563d4daacd5afc8d18d7494a29
gamalmubarak <eng-gemy@hotmail.com>
2025-04-30 21:22:12 -0400

    ADD_USER will execute the addUser mutation

--- Develop/client/src/utils/mutations.ts
+++ Develop/client/src/utils/mutations.ts
@@ -13,4 +13,16 @@ export const LOGIN_USER = gql`
   }
 `;
 
+export const ADD_USER = gql`
+  mutation AddUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        email
+        username
+      }
+    }
+  }
+`;
 
